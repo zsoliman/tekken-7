22201002aa8525a2e9fdd6bad4fa8cc81ffaf22a
Zachary Soliman <zachsoliman1@gmail.com>
2022-09-14 16:07:29 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let fighters = ["akuma", "alisa", "anna", "armor-king", "asuka", "bob", "bryan", "claudio", "devil-jin", "dragunov", "eddy", "eliza"]
+
+const fighterUrl = (name) => {
+    return `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`
+}
